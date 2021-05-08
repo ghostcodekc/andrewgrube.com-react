9888675c97a66c1f2d72ef69f03751b45d4bb71c
ghostcodekc <andrewmgrube@gmail.com>
2021-05-08 10:08:03 -0500

    Uploaded new resume and added dev.to blog post

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -5,8 +5,8 @@ class Resume extends Component {
     if(this.props.data){
       var education = this.props.data.education.map(function(education){
         return <div key={education.school}><h3>{education.school}</h3>
-        <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
-        <p>{education.description}</p></div>
+        <p className="info"><a href={education.description}>{education.degree}</a> <span>&bull;</span><em className="date">{education.graduated}</em></p>
+        </div>
       })
       var work = this.props.data.work.map(function(work, index){
         return <div key={work.company}><h3>{work.company}</h3>
