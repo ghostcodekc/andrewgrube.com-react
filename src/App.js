0f88d44e24ef5321e3cc5300a073b9c163bf11e9
ghostcodekc <andrewmgrube@gmail.com>
2021-07-17 11:48:30 -0500

    Changing profile picture. Fixing resume download.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactGA from 'react-ga';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import $ from 'jquery';
 import './App.css';
 import Header from './Components/Header';
@@ -7,6 +8,7 @@ import Footer from './Components/Footer';
 import About from './Components/About';
 import Resume from './Components/Resume';
 import Portfolio from './Components/Portfolio';
+// import BlogMain from './Components/BlogMain';
 
 class App extends Component {
 
@@ -36,19 +38,35 @@ class App extends Component {
       }
     });
   }
+  getBlogData(){
+    $.ajax({
+      url:'/blogData.json',
+      dataType:'json',
+      cache: false,
+      success: function(data){
+        this.setState({blogData: data});
+      }.bind(this),
+      error: function(xhr, status, err){
+        console.log(err);
+        alert(err);
+      }
+    });
+  }
 
   componentDidMount(){
     this.getResumeData();
+    this.getBlogData();
   }
 
   render() {
     return (
       <div className="App">
-        <Header data={this.state.resumeData.main}/>
-        <About data={this.state.resumeData.main}/>
-        <Resume data={this.state.resumeData.resume}/>
-        <Portfolio data={this.state.resumeData.portfolio}/>
-        <Footer data={this.state.resumeData.main}/>
+        <Router>
+          <Switch>
+          <Route path="/" exact component={() => <><Header data={this.state.resumeData.main}/><About data={this.state.resumeData.main}/><Resume data={this.state.resumeData.resume}/><Portfolio data={this.state.resumeData.portfolio}/><Footer data={this.state.resumeData.main}/></>} />
+          {/* <Route path="/blog" exact component={() => <BlogMain data={this.state.blogData} />} /> */}
+          </Switch>
+        </Router>
       </div>
     );
   }
---
+++ src/Components/BlogPost.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class BlogPost extends Component {
+    render() {
+  
+      if(this.props.data){
+      }
+  
+      return (
+          <div>
+          </div>
+      );
+    }
+  }
+
+  export default BlogPost;
+  
