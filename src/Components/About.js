0532d6c44fc593cfbbf3019a8909c80f648b95f0
Andrew Grube <andrewmgrube@gmail.comgit config user.name ghostcodekc>
2021-10-08 09:59:44 -0500

    bugfix

--- src/Components/About.js
+++ src/Components/About.js
@@ -28,9 +28,9 @@ class About extends Component {
             <div className="row">
                <div className="columns contact-details">
                   <h2>Contact Details</h2>
-                  {/* <p className="address">
+                  <p className="address">
 						   <span>{name}</span><br />
-						   <span>{street}<br />
+						   {/* <span>{street}<br />
 						         {city} {state}, {zip}
                    </span><br /> */}
 						   <span>{phone}</span><br />
